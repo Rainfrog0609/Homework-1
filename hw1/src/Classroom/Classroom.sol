f4803395a1a03098fcf22e96664d75ef4a41addf
Rainfrog0609 <r12922199@ntu.edu.tw>
2024-03-27 01:46:31 +0800

    message

--- hw1/src/Classroom/Classroom.sol
+++ hw1/src/Classroom/Classroom.sol
@@ -1,12 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import {console} from "forge-std/Test.sol";
+
+
 /* Problem 1 Interface & Contract */
 contract StudentV1 {
     // Note: You can declare some state variable
 
+    uint256 private cnt = 0;
+
     function register() external returns (uint256) {
         // TODO: please add your implementaiton here
+
+        if (cnt != 0) return 123;
+
+        cnt++;
+        return 1123;
     }
 }
 
@@ -17,13 +27,17 @@ interface IClassroomV2 {
 
 contract StudentV2 {
     function register() external view returns (uint256) {
-        // TODO: please add your implementaiton here
+        if (!IClassroomV2(msg.sender).isEnrolled()) return 1223;
+        else  return 123;
+        
     }
 }
 
 /* Problem 3 Interface & Contract */
 contract StudentV3 {
     function register() external view returns (uint256) {
-        // TODO: please add your implementaiton here
+        if (gasleft() < 7000)
+            return 123;
+        return 1123; 
     }
 }
